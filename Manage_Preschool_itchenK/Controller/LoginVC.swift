a6f7c56643891ef34003087c29ca63e739a2f129
Luc Thoi Sang <devbrownbear@gmail.com>
2019-04-17 00:40:13 +0700

    Hoan thanh giao dien man hinh dang nhap

--- Manage_Preschool_itchenK/Controller/LoginVC.swift
+++ Manage_Preschool_itchenK/Controller/LoginVC.swift
@@ -14,12 +14,28 @@ class LoginVC: UIViewController {
     let imgBackground = UIImageView()
     let viewBox = UIView()
     let imageLogo = UIImageView()
+    let tfEmployeeCode = UITextField()
+    let tfPassword = UITextField()
+    let stackView = UIStackView()
+    let buttonSignIn = UIButton(type: .system)
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupView()
         eventHandler()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(sender: Notification) {
+        self.view.frame.origin.y = -150
+    }
+    
+    @objc func keyboardWillHide(sender: Notification) {
+        self.view.frame.origin.y = 0
     }
     
     func eventHandler() {
--- Manage_Preschool_itchenK/View/UILogin.swift
+++ Manage_Preschool_itchenK/View/UILogin.swift
@@ -12,26 +12,78 @@ import ChameleonFramework
 
 extension LoginVC {
     
+    // MARK: Thay đổi màu thanh status bar
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
+    // MARK: Hàm setupView liên kết với LoginVC
     func setupView() {
         addUIControls()
         setupUIControls()
         setupConstraints()
     }
     
+    // MARK: Hàm thêm các UIControls vào view
     func addUIControls() {
         view.addSubview(imgBackground)
         view.addSubview(viewBox)
         viewBox.addSubview(imageLogo)
+        viewBox.addSubview(stackView)
+        stackView.addArrangedSubview(tfEmployeeCode)
+        stackView.addArrangedSubview(tfPassword)
+        viewBox.addSubview(buttonSignIn)
     }
     
+    // MARK: Hàm thiết lập các thuộc tính của các UIControls
     func setupUIControls() {
         setupImageBackground()
         setupViewBox()
         setupImageLogo()
+        setupStackView()
+        setupTfEmployeeCode()
+        setupTfPassword()
+        setupButtonSignIn()
+    }
+    
+    func setupButtonSignIn() {
+        buttonSignIn.backgroundColor = UIColor.flatSkyBlue()
+        buttonSignIn.setTitle("Đăng nhập", for: .normal)
+        buttonSignIn.setTitleColor(UIColor(contrastingBlackOrWhiteColorOn: FlatSkyBlue(), isFlat: true), for: .normal)
+        buttonSignIn.layer.cornerRadius = 10
+        buttonSignIn.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setPaddingTextField() -> UIView {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: self.tfEmployeeCode.frame.height))
+        return paddingView
+    }
+    
+    func setupGeneralTextField(tf: UITextField) {
+        tf.layer.cornerRadius = 10
+        tf.backgroundColor = UIColor.flatWhite()
+        tf.leftView = setPaddingTextField()
+        tf.leftViewMode = .always
+        tf.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setupTfPassword() {
+        setupGeneralTextField(tf: tfPassword)
+        tfPassword.placeholder = "Mật khẩu"
+        tfPassword.isSecureTextEntry = true
+        tfPassword.returnKeyType = UIReturnKeyType.go
+    }
+    
+    func setupTfEmployeeCode() {
+        setupGeneralTextField(tf: tfEmployeeCode)
+        tfEmployeeCode.placeholder = "Mã nhân viên"
+    }
+    
+    func setupStackView() {
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 27
+        stackView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func setupImageLogo() {
@@ -43,7 +95,7 @@ extension LoginVC {
     }
     
     func setupViewBox() {
-        viewBox.backgroundColor = UIColor(hexString: "#000", withAlpha: 0.5)
+        viewBox.backgroundColor = UIColor.flatBlack()?.withAlphaComponent(0.7)
         viewBox.layer.cornerRadius = view.frame.width * 0.1
         viewBox.translatesAutoresizingMaskIntoConstraints = false
     }
@@ -53,17 +105,45 @@ extension LoginVC {
         imgBackground.translatesAutoresizingMaskIntoConstraints = false
     }
     
+    // MARK: Hàm thiết lập các ràng buộc của các UIControls trên view
     func setupConstraints() {
         setupImageBackgroundConstraints()
         setupViewBoxConstraints()
         setupImageLogoConstraints()
+        setupStackViewConstraints()
+        setupTfEmployeeCodeConstraints()
+        setupButtonSignInConstraints()
+    }
+    
+    func setupButtonSignInConstraints() {
+        buttonSignIn.snp.makeConstraints { (make) in
+            make.top.equalTo(stackView.snp.bottom).offset(30)
+            make.centerX.equalTo(viewBox)
+            make.width.equalTo(stackView.snp.width).multipliedBy(0.8)
+            make.height.equalTo(50)
+        }
+    }
+    
+    func setupTfEmployeeCodeConstraints() {
+        tfEmployeeCode.snp.makeConstraints { (make) in
+            make.height.equalTo(50)
+        }
+    }
+    
+    func setupStackViewConstraints() {
+        stackView.snp.makeConstraints { (make) in
+            make.top.lessThanOrEqualTo(imageLogo.snp.bottom).offset(40)
+            make.left.equalToSuperview().offset(30)
+            make.right.equalToSuperview().offset(-30)
+            make.height.equalToSuperview().multipliedBy(0.25)
+        }
     }
     
     func setupImageLogoConstraints() {
         imageLogo.snp.makeConstraints { (make) in
             make.centerX.equalToSuperview()
             make.top.equalToSuperview().offset(37)
-            make.size.equalTo(110)
+            make.size.lessThanOrEqualTo(110)
         }
     }
     
@@ -77,7 +157,7 @@ extension LoginVC {
         viewBox.snp.makeConstraints { (make) in
             make.center.equalToSuperview()
             make.width.equalTo(view.snp.width).multipliedBy(0.75)
-            make.height.equalTo(view.snp.height).multipliedBy(0.5)
+            make.bottom.equalTo(buttonSignIn.snp.bottom).offset(20)
         }
     }
     
