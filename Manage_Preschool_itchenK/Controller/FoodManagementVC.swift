b74bf5a79d560f42cc32ba8d91e1e1ab4c326da6
Vinh Trần <vinhtran@Vinhs-MBP.net.fpt>
2019-04-20 22:32:40 +0700

    Them gridview vao FoodManagement

--- Manage_Preschool_itchenK/Controller/FoodManagementVC.swift
+++ Manage_Preschool_itchenK/Controller/FoodManagementVC.swift
@@ -10,23 +10,120 @@ import UIKit
 
 
 class FoodManagementVC: UIViewController {
-
+    
     let imgBackground = UIImageView()
     let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
     let blurView = UIVisualEffectView()
-    let topViewBox = UIView()
-    let signOut = UIImageView()
-    let add = UIImageView()
-    let tfDashBoard = UILabel()
+    let categoryLabel = UILabel()
+    
+    let FMCollectionView: UICollectionView = {
+        let layout=UICollectionViewFlowLayout()
+        let collection = UICollectionView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), collectionViewLayout: layout)
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 30
+        layout.minimumLineSpacing = 30
+        collection.backgroundColor=UIColor.black
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.isScrollEnabled = true
+        return collection
+        }()
+
+    let cellIdentifier="itemCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         setupView()
-        eventHandler()
+        //eventHandler()
+        FMCollectionView.dataSource = self
+        FMCollectionView.delegate = self
+        view.addSubview(FMCollectionView)
+        FMCollectionView.register(UIFMCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        setupCollection()
+    }
+    func setupCollection(){
+        
+        FMCollectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        FMCollectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        FMCollectionView.heightAnchor.constraint(equalToConstant: 467  ).isActive = true
+        FMCollectionView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
     }
 
-    func eventHandler() {
-        print("456")
-    }
+//    func eventHandler() {
+//        print("456")
+//    }
+    
+}
 
+extension FoodManagementVC: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout  {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let customCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! UIFMCell
+        customCell.backgroundColor  = .white
+        customCell.layer.cornerRadius = 20
+        customCell.layer.shadowOpacity = 3
+        customCell.layer.masksToBounds = true
+        customCell.cellImage.image = UIImage(named: "BitmapCơm")
+
+        return customCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 120, height: 170)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 25, bottom: 35, right: 25)
+    }
+    
+}
+
+class UIFMCell: UICollectionViewCell {
+    override init (frame: CGRect){
+        super.init(frame: frame)
+        
+        setupView()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let cellImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.clipsToBounds = true
+        return image
+    }()
+    
+    let textLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.textColor = .black
+        label.text = "Cơm"
+        return label
+    }()
+    
+    func setupView(){
+        addSubview(cellImage)
+        addSubview(textLabel)
+        
+        cellImage.snp.makeConstraints { (make) in
+            make.left.right.top.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.8)
+        }
+        
+        textLabel.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.2)
+        }
+    }
 }
--- Manage_Preschool_itchenK/View/UIFoodManagement.swift
+++ Manage_Preschool_itchenK/View/UIFoodManagement.swift
@@ -15,14 +15,16 @@ extension FoodManagementVC {
     func setupView() {
         configNavController()
         addUIControls()
+        setupGeneral()
         setupUIControls()
         setupConstraints()
-        setupGeneral()
+        
     }
     
     func configNavController(){
         navigationItem.title = "Quản lý món ăn"
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "icons8-exit"), style: .plain, target: self, action: #selector(buttonSignOutPressed))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "icons8-plus_math_filled"), style: .plain, target: self, action: #selector(buttonAddPressed))
     }
     
     @objc func buttonSignOutPressed(){
@@ -31,25 +33,40 @@ extension FoodManagementVC {
         present(loginVC, animated: true, completion: nil)
     }
     
-    
+    @objc func buttonAddPressed(){
+        
+    }
     
     func addUIControls() {
         //view.addSubview(imgBackground)
         //view.addSubview(blurView)
+        //view.addSubview(FMCollectionView)
     }
     
     func setupUIControls() {
         //setupImageBackground()
         //setupBlurView()
-
+        //setupCollectionView()
     }
     
     
     func setupConstraints() {
-        
+        //setupCollectionViewConstraints()
+    }
+    
+    func setupCollectionView(){
+        //FMCollectionView.backgroundColor=UIColor.black
+        //FMCollectionView.numberOfItems(inSection: 2)
+    }
+    
+    func setupCollectionViewConstraints(){
+//        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+//        layout.itemSize = CGSize(width: 60, height: 60)
+//        let FMCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
     }
     
     func setupGeneral(){
         view.backgroundColor = UIColor.white
     }
+    
 }
